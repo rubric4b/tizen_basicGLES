afbc8315b1ccf0efb7fd57ec65a170761280b337
Grayish <hobbangz5@gmail.com>
2015-11-10 11:34:45 +0900

    update appdata_s

--- inc/basicgles.h
+++ inc/basicgles.h
@@ -16,16 +16,40 @@
 #define PACKAGE "org.example.basicgles"
 #endif
 
-typedef struct appdata
-{
-	const char* name;
+typedef struct appdata {
+	const char *name;
 
 	// All graphical objects here are pointers to the value of the type Evas_Object
 	Evas_Object *win;
 	Evas_Object *conform;
 	Evas_Object *naviframe;
 
+	Evas_Object *fps_label;
+	Ecore_Animator *ani;
+
+	int glview_w, glview_h;
+
+	/* GL related data here... */
+	unsigned int program;
+	unsigned int vtx_shader;
+	unsigned int fgmt_shader;
+
+	float xangle;
+	float yangle;
+
+	unsigned int idx_position;
+	unsigned int idx_color;
+	int idx_mvp;
+
+	float mvp[16];
+
+	Eina_Bool mouse_down : 1;
+	Eina_Bool initialized :1;
 } appdata_s;
+
+// in gl.c
+Evas_Object*
+		add_glview(Evas_Object* parent, appdata_s *ad);
 
 
 #endif /* __basicgles_H__ */
